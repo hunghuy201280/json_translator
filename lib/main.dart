cdac52bb8f201f5844638aaaa86abba2b3b0edd9
Truong Hung Huy <humghuy201280@gmail.com>
2022-09-21 15:22:07 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
-import 'package:json_translator/translate_app.dart';
 import 'package:flutter/material.dart';
+import 'package:json_translator/translate_app.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Must add this line.
+  await windowManager.ensureInitialized();
 
+  WindowOptions windowOptions = const WindowOptions(
+    fullScreen: true,
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
   runApp(const TranslateApp());
 }
